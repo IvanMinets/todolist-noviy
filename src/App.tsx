36fa8d8e0914a3318c5c20ee976ce8804ff6b4ce
Ivan Minets2 <minetssmash@gmail.com>
2023-04-26 18:18:39 +0300

    Added filter for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import Todolist from "./Todolist";
 
+export type FilterValuesType = "all" | "completed" | "active"
+
 function App() {
-    let tasks1 = [
+    const [tasks, setTasks] = useState([
         {id: 1, title: "CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: false}
-    ]
-    let tasks2 = [
-        {id: 1, title: "Terminator", isDone: true},
-        {id: 2, title: "XXX", isDone: true},
-    ]
+    ]);
+    const [filter, setFilter] = useState<FilterValuesType>("all");
+
+    const removeTask = (id: number) => {
+        let resultTasks = tasks.filter(t => t.id !== id) // t - параметр функции.
+        // Здесь сокращённая конструкция if-else. if (t.id !== id) {return true} else {return false}
+        setTasks(resultTasks); //setTask часть хука useState отвечает за перерисовку нового массива с тасками после удаления
+    }
+    const changeFilter = (value: FilterValuesType) => {
+        setFilter(value)
+    } // Функция для фильтрации тасок. setFilter Отрисовывыает
+
+    let tasksForTodoList = tasks;
+    if (filter === "completed") {
+        tasksForTodoList = tasks.filter(t => t.isDone === true)
+    }
+    if (filter === "active") {
+        tasksForTodoList = tasks.filter(t => t.isDone === false)
+    }
 
     return (
         <div className="App">
-            <Todolist title={"What to learn"} tasks = {tasks1}/>
-            <Todolist title={"What to buy"} tasks = {tasks2}/>
+            <Todolist title={"What to learn"}
+                      tasks={tasksForTodoList}
+                      removeTask={removeTask}
+                      changeFilter={changeFilter}
+            />
         </div>
     );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FilterValuesType} from "./App";
 
 type TaskType = {
     id: number
@@ -8,6 +9,8 @@ type TaskType = {
 type PropsType = {
     title: string
     tasks: Array<TaskType>
+    removeTask: (id: number) => void
+    changeFilter: (value: FilterValuesType) => void
 }
 
 
@@ -24,14 +27,14 @@ function Todolist(props: PropsType) {
                     //убрали return и скобки .map((t)=>{return <li>...}
                     props.tasks.map(t => <li><input type="checkbox" checked={t.isDone}/>
                         <span>{t.title}</span>
-                        <button>x</button>
+                        <button onClick={ () => {props.removeTask(t.id)} }>x</button>
                     </li>) //map - это метод массива, который на основе каждого элемента в массиве, создаёт новый элемент;
                 }
             </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={() => {props.changeFilter("all")}}>All</button>
+                <button onClick={()=>{props.changeFilter("active")}}>Active</button>
+                <button onClick={()=>{props.changeFilter("completed")}}>Completed</button>
             </div>
         </div>
     )
