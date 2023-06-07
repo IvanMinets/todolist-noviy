d00ff848424fe22f70211ce005449a9cd990c73b
Ivan Minets2 <minetssmash@gmail.com>
2023-06-07 15:19:58 +0300

    src update

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import './App.css';
 import {TaskType, Todolist} from './Todolist';
 import {v1} from 'uuid';
 import {AddItemForm} from './AddItemForm';
-import {AppBar, Button, Container, Grid, Paper, Toolbar, Typography} from "@mui/material";
-import IconButton from "@mui/material/IconButton/IconButton";
+import {AppBar, Button, Container, Grid, IconButton, Paper, Toolbar, Typography} from "@mui/material";
 import {Menu} from "@mui/icons-material";
-import {useDispatch} from "react-redux";
 
 
 export type FilterValuesType = "all" | "active" | "completed";
@@ -22,7 +20,6 @@ export type TasksStateType = {
 
 
 function App() {
-    const dispatch = useDispatch();
     let todolistId1 = v1();
     let todolistId2 = v1();
 
@@ -41,7 +38,6 @@ function App() {
             {id: v1(), title: "React Book", isDone: true}
         ]
     });
-
 
     function removeTask(id: string, todolistId: string) {
         //достанем нужный массив по todolistId:
@@ -88,7 +84,6 @@ function App() {
         }
     }
 
-
     function changeFilter(value: FilterValuesType, todolistId: string) {
         let todolist = todolists.find(tl => tl.id === todolistId);
         if (todolist) {
@@ -97,9 +92,14 @@ function App() {
         }
     }
 
-    // function removeTodolist(id: string) {
-    // //    dispatch(removeTodolist(id))
-    // // }
+    function removeTodolist(id: string) {
+        // засунем в стейт список тудулистов, id которых не равны тому, который нужно выкинуть
+        setTodolists(todolists.filter(tl => tl.id != id));
+        // удалим таски для этого тудулиста из второго стейта, где мы храним отдельно таски
+        delete tasks[id]; // удаляем св-во из объекта... значением которого являлся массив тасок
+        // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
+        setTasks({...tasks});
+    }
 
     function changeTodolistTitle(id: string, title: string) {
         // найдём нужный todolist
